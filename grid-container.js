c4071ada4837148378c9cbc4991c3a75a15bfe62
Charbel <charbel.f.rami@gmail.com>
2017-03-30 16:46:41 -0300

    remove unused code

--- grid-container.js
+++ grid-container.js
@@ -22,58 +22,13 @@ customElements.define(
         </style>
         <slot></slot>`;
     }
+
     static get observedAttributes() {
       return ['gutter', 'areas', 'grid'];
     }
 
-    get areas() {
-      return this.hasAttribute('areas');
-    }
-
-    set areas(val) {
-      if (val) {
-        this.setAttribute('areas', val);
-      } else {
-        this.removeAttribute('areas');
-      }
-    }
-
-    get grid() {
-      return this.hasAttribute('grid');
-    }
-
-    set grid(val) {
-      if (val) {
-        this.setAttribute('grid', val);
-      } else {
-        this.removeAttribute('grid');
-      }
-    }
-
-    get gutter() {
-      return this.hasAttribute('gutter');
-    }
-
-    set gutter(val) {
-      if (val) {
-        this.setAttribute('gutter', val);
-      } else {
-        this.removeAttribute('gutter');
-      }
-    }
-
     attributeChangedCallback(name, oldVal, newVal) {
-      if (name === 'gutter') {
-        this.style.setProperty('--gutter', newVal);
-      }
-
-      if (name === 'areas') {
-        this.style.setProperty('--areas', newVal);
-      }
-
-      if (name === 'grid') {
-        this.style.setProperty('--grid', newVal);
-      }
+      this.style.setProperty(`--${name}`, newVal);
     }
   }
 );
@@ -97,56 +52,13 @@ customElements.define(
         </style>
         <slot></slot>`;
     }
+
     static get observedAttributes() {
       return ['area', 'row', 'column'];
     }
 
-    get area() {
-      return this.hasAttribute('area');
-    }
-
-    set area(val) {
-      if (val) {
-        this.setAttribute('area', val);
-      } else {
-        this.removeAttribute('area');
-      }
-    }
-
-    get row() {
-      return this.hasAttribute('row');
-    }
-
-    set row(val) {
-      if (val) {
-        this.setAttribute('row', val);
-      } else {
-        this.removeAttribute('row');
-      }
-    }
-
-    get column() {
-      return this.hasAttribute('column');
-    }
-
-    set column(val) {
-      if (val) {
-        this.setAttribute('column', val);
-      } else {
-        this.removeAttribute('column');
-      }
-    }
-
     attributeChangedCallback(name, oldVal, newVal) {
-      if (name === 'area') {
-        this.style.setProperty('--area', newVal);
-      }
-      if (name === 'row') {
-        this.style.setProperty('--row', newVal);
-      }
-      if (name === 'column') {
-        this.style.setProperty('--column', newVal);
-      }
+      this.style.setProperty(`--${name}`, newVal);
     }
   }
 );
